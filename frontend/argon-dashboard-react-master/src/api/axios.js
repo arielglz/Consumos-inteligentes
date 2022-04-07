ceb589e7e31f577f813043f2a9eaaa0e49ace421
Ariel Gonzalez <a.gonzalez128@unapec.edu.do>
2022-04-07 15:59:35 -0400

    modifying axios

--- frontend/argon-dashboard-react-master/src/api/axios.js
+++ frontend/argon-dashboard-react-master/src/api/axios.js
@@ -3,4 +3,5 @@ import axios from 'axios';
 export default axios.create({
     //baseURL: 'http://10.0.0.3:3000/'
     baseURL: 'http://localhost:4000/'
+    //baseURL: 'http://172.20.10.6:4000/'
 })
